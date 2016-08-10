072b0558732d63264dec5c5ee9e2101a7dc3028a
Niklas Schreiber <niklas@derschreiber.com>
2016-08-10 09:56:26 +0200

    remove pony

--- lib/locomotive/liquid_extensions.rb
+++ lib/locomotive/liquid_extensions.rb
@@ -1,6 +1,4 @@
 require 'solid'
-require 'pony'
-require 'net/http'
 
 %w{misc tags filters}.each do |dir|
   Dir[File.join(File.dirname(__FILE__), 'liquid_extensions', dir, '*.rb')].each { |lib| require lib }
