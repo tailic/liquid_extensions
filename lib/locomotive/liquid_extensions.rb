3ae5409222e4213d218797219196999e62bcce1c
Niklas Schreiber <niklas@derschreiber.com>
2016-08-10 09:53:28 +0200

    only load own custom filters

--- lib/locomotive/liquid_extensions.rb
+++ lib/locomotive/liquid_extensions.rb
@@ -6,11 +6,11 @@ require 'net/http'
   Dir[File.join(File.dirname(__FILE__), 'liquid_extensions', dir, '*.rb')].each { |lib| require lib }
 end
 
-Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Number)
-Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Math)
-Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Sample)
-Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Json)
-Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Hexdigest)
+#Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Number)
+#Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Math)
+#Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Sample)
+#Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Json)
+#Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Hexdigest)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Md5)
 # DEBUG
 # puts "[LocomotiveLiquidExtensions] tags and filters loaded"
